76c287c46aab1feddcb85c39e1a0ab146c386cd8
Victor Romero <romeroa.victor@yahoo.com>
2020-08-27 22:26:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,16 +1,26 @@
 game_hash={
   :home={
     :team_name=>"Brooklyn Nets"
-    :colors=>[]
-    :players=>[{:player_name=>},
-      {:number=>},
-      {:shoe=>},
-      {:points=>},
-      {:rebounds=>},
-      {:assists=>},
-      {:steals=>},
-      {:bloack=>},
-      {:slam_dunks=>}]
+    :colors=>["Black", "White"]
+    :players=>[
+     {:player_name=>"Alan Anderson",
+      :number=>0,
+      :shoe=>16,
+      :points=>22,
+      :rebounds=>12,
+      :assists=>12,
+      :steals=>3,
+      :blocks=>1,
+      :slam_dunks=>1},
+     {:player_name=>"Reggie Evans",
+      :number=>30,
+      :shoe=>14,
+      :points=>12,
+      :rebounds=>12,
+      :assists=>12,
+      :steals=>12,
+      :blocks=>12,
+      :slam_dunks=>7},]
     }
   :away={
     :team_name=>""
