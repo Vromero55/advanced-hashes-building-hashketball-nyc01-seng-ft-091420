2c988d7efcf6d5bbd0d8b80f57ffcbaba018eaff
Victor Romero <romeroa.victor@yahoo.com>
2020-08-27 22:16:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,12 +1,29 @@
 game_hash={
   :home={
-    :team_name=>""
+    :team_name=>"Brooklyn Nets"
     :colors=>[]
-    :players=>[{}]
-  }
+    :players=>[{:player_name=>},
+      {:number=>},
+      {:shoe=>},
+      {:points=>},
+      {:rebounds=>},
+      {:assists=>},
+      {:steals=>},
+      {:bloack=>},
+      {:slam_dunks=>}]
+    }
   :away={
     :team_name=>""
     :colors=>[]
-    :players=>[{}]
+    :players=>[
+      {:player_name=>},
+      {:number=>},
+      {:shoe=>},
+      {:points=>},
+      {:rebounds=>},
+      {:assists=>},
+      {:steals=>},
+      {:blocks=>},
+      {:slam_dunks=>}]
   }
 }
